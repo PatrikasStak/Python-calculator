3efe6a506f4ebd597adea96a6813dcb991a72e29
Patrikas Stakenas <patrestak@gmail.com>
2025-11-10 21:41:34 +0200

    9 buttons

--- calculator.py
+++ calculator.py
@@ -22,13 +22,32 @@ def evaluate_calculation(): #can be injected into??
         
 
 def clear_field():
-    pass
+    global calculation
+    calculation = ""
+    text_result.delete(1.0, "end")
 
 root = tk.Tk()
 root.geometry("300x275")
 
 text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
 text_result.grid(columnspan=5)
-
+btn1= tk.Button(root, text="1", command=lambda: add_to_calculation(1), width=5, font=("Arial", 14))
+btn1.grid(row=2, column=1)
+btn2= tk.Button(root, text="2", command=lambda: add_to_calculation(2), width=5, font=("Arial", 14))
+btn2.grid(row=2, column=2)
+btn3= tk.Button(root, text="3", command=lambda: add_to_calculation(3), width=5, font=("Arial", 14))
+btn3.grid(row=2, column=3)
+btn4= tk.Button(root, text="4", command=lambda: add_to_calculation(4), width=5, font=("Arial", 14))
+btn4.grid(row=3, column=1)
+btn5= tk.Button(root, text="5", command=lambda: add_to_calculation(5), width=5, font=("Arial", 14))
+btn5.grid(row=3, column=2)
+btn6= tk.Button(root, text="6", command=lambda: add_to_calculation(6), width=5, font=("Arial", 14))
+btn6.grid(row=3, column=3)
+btn7= tk.Button(root, text="7", command=lambda: add_to_calculation(7), width=5, font=("Arial", 14))
+btn7.grid(row=4, column=1)
+btn8= tk.Button(root, text="8", command=lambda: add_to_calculation(8), width=5, font=("Arial", 14))
+btn8.grid(row=4, column=2)
+btn9= tk.Button(root, text="9", command=lambda: add_to_calculation(9), width=5, font=("Arial", 14))
+btn9.grid(row=4, column=3)
 root.mainloop()
 
