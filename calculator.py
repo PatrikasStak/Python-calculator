56218cfa2284a122e9c5f2d3c122e3bf98b3d4a3
Patrikas Stakenas <patrestak@gmail.com>
2025-11-10 21:32:32 +0200

    2 functions done i think

--- calculator.py
+++ calculator.py
@@ -3,10 +3,23 @@ import tkinter as tk
 calculation = ""
 
 def add_to_calculation(symbol):
-    pass
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0, "end")
+    text_result.insert(1.0, calculation)
 
-def evaluate_calculation():
-    pass
+def evaluate_calculation(): #can be injected into??
+    global calculation
+    try:
+        calculation=str(eval(calculation))
+        calculation=""
+        text_result.delete(1.0, "end")
+        text_result.insert(1.0, calculation)
+
+    except:
+        clear_field()
+        text_result.insert(1.0, "Error")
+        
 
 def clear_field():
     pass
