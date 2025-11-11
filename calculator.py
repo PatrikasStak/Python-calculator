1d9045b92efceca7e1a8e56441afe1ddf5414fb5
Patrikas Stakenas <patrestak@gmail.com>
2025-11-11 14:05:57 +0200

    Gave title and fixed dimentions

--- calculator.py
+++ calculator.py
@@ -31,7 +31,9 @@ def clear_field():
     text_result.delete(1.0, "end")
 
 root = tk.Tk()
-root.geometry("300x275")
+root.title("Calculator")
+root.geometry("365x275")
+root.resizable(False, False)
 
 text_result = tk.Text(root, height=2, width=16, font=("Arial", 24))
 text_result.grid(columnspan=5)
